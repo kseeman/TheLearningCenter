70045fec516a96a8b732d4d654b4487ac55f4198
Yolanda Haynes <yhaynes@giyo.us>
2023-10-18 16:03:40 -0700

    added login page

--- WorstReactApp/App.tsx
+++ WorstReactApp/App.tsx
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native';
 import MyNewPage from './MyNewPage';
 import Home from './Home';
+import Login from './Login';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -12,6 +13,7 @@ export default function App() {
       <Stack.Navigator>
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="MyNewPage" component={MyNewPage} />
+        <Stack.Screen name="Login" component={Login} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ WorstReactApp/Login.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function Login() {
+  return (
+    <View>
+      <Text>Username</Text>
+      <Text>Password</Text>
+    
+    </View>
+  );
+}
