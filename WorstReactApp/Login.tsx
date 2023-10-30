a7a3f1f4f63f9a643ff91b35f1cf20c68ec8f829
Yolanda Haynes <yhaynes@giyo.us>
2023-10-30 16:21:17 -0700

    btn, textinput, nav to login section

--- WorstReactApp/Login.tsx
+++ WorstReactApp/Login.tsx
@@ -1,12 +1,33 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TextInput } from "react-native";
 
 export default function Login() {
+  const [text, onChangeText] = React.useState('');
+  const [passwordtext, onChangePassword] = React.useState('');
+
   return (
     <View>
-      <Text>Username</Text>
-      <Text>Password</Text>
+      <TextInput         
+          style={{
+          height: 40,
+          borderColor: 'red',
+          borderWidth: 1,
+        }}
+        onChangeText={onChangeText}
+        value={text}
+        placeholder="Username"></TextInput>
+      <TextInput
+          style={{
+            height: 40,
+            borderColor: 'blue',
+            borderWidth: 1,
+          }}
+          onChangeText={onChangePassword}
+          value={passwordtext}
+          placeholder="password"
+          secureTextEntry={true}></TextInput>
     
     </View>
   );
-}
+};
+
